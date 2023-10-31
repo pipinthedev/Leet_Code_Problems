4ef0e72ff688e9fa4c73a991f8f079e8723505ac
PIPIN <pipin.gitacc@outlook.com>
2023-11-01 05:15:07 +0530

    added lines.js to print number

--- line.js
+++ line.js
@@ -1,3 +1,5 @@
+// Write a code to get 2 integers A and N. Print the integer A, N times in separate line.
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -5,6 +7,8 @@ const readline = require('readline').createInterface({
 
 readline.question('Enter the Number 1 :', num1 => {
 readline.question('Enter the Number 2 :', num2 => {
-    console.log(num1,num2);
+    for (let i = 0; i <= num2; i++) {
+        console.log(num1);
+    }
   });
 });

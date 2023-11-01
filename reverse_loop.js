00015c3826963dd34e1fa820f65d7fa4f49057eb
PIPIN <pipin.gitacc@outlook.com>
2023-11-01 05:41:15 +0530

    reverse loop

--- reverse_loop.js
+++ reverse_loop.js
@@ -1,10 +1,14 @@
+//Write a code to get an integer N and print the values from N to 1.
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 readline.question('Enter the Number 1 :', num1 => {
-readline.question('Enter the Number 2 :', num2 => {
-    console.log(num1,num2);
-});
+    var x = num1;
+ for (let i = x; i > 0;--i)
+ {
+    console.log(i);
+ }
 });

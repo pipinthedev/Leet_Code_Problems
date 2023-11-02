3faf0b7b2a3da1820b513ed3840514251ebf6e39
PIPIN <pipin.gitacc@outlook.com>
2023-11-03 05:27:40 +0530

    added dates

---
+++ 03-11-2023/dates.js
@@ -0,0 +1,17 @@
+
+var ok = new Date;
+var year = ok.getFullYear();
+var month = ok.getMonth();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var day = ok.getDay();
+var seconds = ok.getSeconds();
+console.log("Today is : " + daylist[day] + ".");
+console.log(day+' '+year+' '+month+ ' '+seconds);
+
+if (year % 4 == 0) { 
+    console.log("This year is leap year");
+}
+else 
+{
+  console.log("This year is Non leap year");
+}
